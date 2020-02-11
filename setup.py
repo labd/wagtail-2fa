c4cb8886e0af429f84d4b6660012a541c9782b0b
Michiel Bijland <MichielBijland@users.noreply.github.com>
2020-02-11 09:20:04 +0100

    corrected flake8-debugger version bump
    flake8-debugger was bumped to 1.4.1 and this release doesn't exist reverted to 1.4.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ tests_require = [
     'isort==4.2.5',
     'flake8==3.0.3',
     'flake8-blind-except==0.1.1',
-    'flake8-debugger==1.4.1',
+    'flake8-debugger==1.4.0',
 ]
 
 with open('README.rst') as fh:
