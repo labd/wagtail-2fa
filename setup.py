d08515d5bbf8670423dcd13e6b32d8b4896d2663
Nando Davis <nandodavis@gmail.com>
2022-09-20 13:28:39 +0200

    Update version

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ with open("README.rst") as fh:
 
 setup(
     name="wagtail-2fa",
-    version="1.6.0",
+    version="1.6.3",
     description="Two factor authentication for Wagtail",
     long_description=long_description,
     url="https://github.com/LabD/wagtail-2fa",
