848c77014656e63b0326ab35c54aaf576f769d82
Nando Davis <nandodavis@gmail.com>
2023-12-20 12:21:51 +0100

    Update changelog for 1.6.8

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ with open("README.rst") as fh:
 
 setup(
     name="wagtail-2fa",
-    version="1.6.7",
+    version="1.6.8",
     description="Two factor authentication for Wagtail",
     long_description=long_description,
     url="https://github.com/LabD/wagtail-2fa",
