e1a09c24ad8123cd7de207f7cdeca044daa8125a
Leon Graumans <l.graumans@labdigital.nl>
2025-07-24 10:16:17 +0200

    Bump version to 1.7.0

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ with open("README.rst") as fh:
 
 setup(
     name="wagtail-2fa",
-    version="1.6.9",
+    version="1.7.0",
     description="Two factor authentication for Wagtail",
     long_description=long_description,
     url="https://github.com/LabD/wagtail-2fa",
